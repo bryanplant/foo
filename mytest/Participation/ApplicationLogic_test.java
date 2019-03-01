1f112cde7b8657c4d126d404ad99dc9d02b3f3a5
Bryan Plant <bryanplant@gmail.com>
2019-03-01 15:35:29 -0700

    final tests

--- mytest/Participation/ApplicationLogic_test.java
+++ mytest/Participation/ApplicationLogic_test.java
@@ -1,6 +1,10 @@
 package Participation;
-import org.junit.* ;
-import static org.junit.Assert.* ;
+
+import org.junit.*;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 /**
  * This is just a simple template for Junit test-class for testing
@@ -10,58 +14,54 @@ import static org.junit.Assert.* ;
  * reset the content of the database to the value it had, before
  * the test case. Otherwise, multiple test cases will interfere
  * with each other, which makes debugging hard should you find error.
- * 
  */
 public class ApplicationLogic_test {
 
-	/**
-	 * Provide a functionality to reset the database. Here I will just
-	 * delete the whole database file. 
-	 */
-	private void setupDB() {
-		Persistence.wipedb() ;
-	}
-	
-	
-	@Test
-	public void test1() {
-		// We'll always begin by reseting the database. This makes sure
-		// the test start from a clean, well defined state of the database.
-		// In this case it would be just an empty database, though it 
-		// doesn't have to be like that.
-		setupDB() ;
-		
-		System.out.println("** Testing add customer...") ;
-		
-		// Creating the target thing you want to test:
-		ApplicationLogic SUT = new ApplicationLogic() ;
-		
-		// Now let's perform some testing. If we add a customer to the system,
-		// test that this customer should then be really added to the system:
-		int duffyID = SUT.addCustomer("Duffy Duck", "") ;
-		Customer C = SUT.findCustomer(duffyID) ;
-		assertTrue(C.name.equals("Duffy Duck")) ;
-		assertTrue(C.email.equals("")) ;		
-	}
-	
-	// Another example...
-	@Test
-	public void test2() {
-		setupDB() ;
-		ApplicationLogic SUT = new ApplicationLogic() ;
-		
-		System.out.println("** Testing getCostToPay ...") ;
-		
-		int duffyID = SUT.addCustomer("Duffy Duck", "") ;
-		int flowerServiceID = SUT.addService("Flowers online shop", 100) ;
-		// let Duffy but 2x participations on Flower-online:
-		SUT.addParticipation(duffyID, flowerServiceID) ;
-		SUT.addParticipation(duffyID, flowerServiceID) ;
+    /**
+     * Provide a functionality to reset the database. Here I will just
+     * delete the whole database file.
+     */
+    private void setupDB() {
+        Persistence.wipedb();
+    }
 
-		// Now let's check if the system correctly calculates the participation
-		// cost of Duffy:
-		Customer C = SUT.findCustomer(duffyID) ;
-		assertTrue(C.getCostToPay() == 200) ;
-	}
-	
+    @Test
+    public void testRemoveService() {
+        setupDB();
+        ApplicationLogic SUT = new ApplicationLogic();
+
+        int cID = SUT.addCustomer("Test Customer", "TestCustomer@test.org");
+        int sID = SUT.addService("Service1", 100);
+        SUT.addParticipation(cID, sID);
+
+        // check that the participation value is correct and the service exists
+        assertEquals(SUT.participationValue(cID), 100);
+        assertTrue(SUT.serviceExists(sID));
+        SUT.removeService(sID);
+        // check that there is no participation value and the service is deleted
+        assertEquals(SUT.participationValue(cID), 0);
+        assertFalse(SUT.serviceExists(sID));
+    }
+
+    @Test
+    public void testResolve() {
+        setupDB();
+        ApplicationLogic SUT = new ApplicationLogic();
+
+        int cID1 = SUT.addCustomer("Test Customer1", "TestCustomer1@test.org");
+        int sID1 = SUT.addService("Service1", 100000);
+        int cID2 = SUT.addCustomer("Test Customer2", "TestCustomer2@test.org");
+        int sID2 = SUT.addService("Service2", 1000);
+
+        // add the participations and give customer 1 a discount
+        SUT.addParticipation(cID1, sID1);
+        SUT.addParticipation(cID2, sID2);
+        SUT.awardDiscount(cID1, ApplicationLogic.D1000);
+
+        // verify that both participation values exist and the discount was applied
+        Map<Customer, Integer> results = SUT.resolve();
+        assertEquals(results.size(), 2);
+        assertTrue(results.containsValue(95000));
+        assertTrue(results.containsValue(1000));
+    }
 }
--- mytest/Participation/Discount_1000_test.java
+++ mytest/Participation/Discount_1000_test.java
@@ -8,11 +8,41 @@ public class Discount_1000_test {
 
     @Test
     public void testApplicable() {
+        Discount_1000 d = new Discount_1000();
 
+        // less than 1000 participation value
+        Customer c = new Customer(123, "Test Customer", "TestCustomer@test.org");
+        Service s1 = new Service(1, "Service1", 50000);
+        Service s2 = new Service(2, "Service2", 10000);
+        Participation p1 = new Participation(c, s1);
+        Participation p2 = new Participation(c, s2);
+        c.participations.add(p1);
+        c.participations.add(p2);
+
+        boolean applicable = d.applicable(c);
+        assertFalse(applicable);
+
+        // greater than 1000 participation value
+        Service s3 = new Service(2, "Service2", 450000);
+        Participation p3 = new Participation(c, s3);
+        c.participations.add(p3);
+
+        applicable = d.applicable(c);
+        assertTrue(applicable);
     }
 
     @Test
     public void testCalcDiscount() {
+        Discount_1000 d = new Discount_1000();
+        Customer c = new Customer(123, "Test Customer", "TestCustomer@test.org");
+        Service s1 = new Service(1, "Service1", 500000);
+        Service s2 = new Service(2, "Service2", 500000);
+        Participation p1 = new Participation(c, s1);
+        Participation p2 = new Participation(c, s2);
+        c.participations.add(p1);
+        c.participations.add(p2);
 
+        int discount_value = d.calcDiscount(c);
+        assertEquals(discount_value, 50000);
     }
 }
